b0d417baba15f6c41fc67b09749471d2fc45c298
Nikita-Astafyev <136175861+Nikita-Astafyev@users.noreply.github.com>
2025-08-07 11:13:29 +0300

    feat: add reverse proxy for user and product services

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,38 @@ package server
 
 import (
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/Nikita-Astafyev/api-gateway-go/internal/auth"
 )
 
-func StartServer() {
-	http.HandleFunc("/api/users", userHandler)
-	http.HandleFunc("/api/products", productHandler)
+func userProxy() http.Handler {
+	target, _ := url.Parse("http://localhost:8081")
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	proxy.Director = func(r *http.Request) {
+		r.URL.Scheme = target.Scheme
+		r.URL.Host = target.Host
+		r.URL.Path = "/"
+		r.Host = target.Host
+	}
+
+	return proxy
 }
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
+func productProxy() http.Handler {
+	target, _ := url.Parse("http://localhost:8082")
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	return proxy
+}
+
+func StartServer() {
+	http.Handle("/api/users", auth.APIKeyMiddleware(userProxy()))
+	http.Handle("/api/products", auth.APIKeyMiddleware(productProxy()))
+}
+
+/*func userHandler(w http.ResponseWriter, r *http.Request) {
 	auth.APIKeyMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("User Service (TODO)"))
 	})(w, r)
@@ -19,4 +41,4 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Product Service (TODO: Forward to real service)"))
-}
+}*/
